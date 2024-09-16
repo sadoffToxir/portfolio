a3eb4f2b87e6a0986367ec9280698fd0dcb695bc
sadoffToxir <toxa.saidoff@gmail.com>
2024-09-16 21:27:45 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import CoreNavbar from './components/Core/CoreNavbar.js'
 import StarsCanvas from './components/Core/Stars/CoreStars.js'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#242424',
+      paper: '#242424',
+    }
+  },
+});
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='w-full h-full'>
+    <ThemeProvider theme={darkTheme}>
+      <div>
+      <CssBaseline />
+
+      <CoreNavbar />
       <StarsCanvas />
-    </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
